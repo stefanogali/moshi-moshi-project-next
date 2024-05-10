77c924a09d87b93bfa61c6429f7c91fbd273f148
Stefano Galiffa <paninos79@yahoo.it>
2024-05-10 11:17:58 +0100

    Remove console.logs and uninstall package

--- helper-functions/db-connection.js
+++ helper-functions/db-connection.js
@@ -1,28 +1,5 @@
-// import mysql from "serverless-mysql";
-
-// const db = mysql({
-// 	config: {
-// 		host: process.env.MYSQL_HOST,
-// 		port: process.env.MYSQL_PORT,
-// 		database: process.env.MYSQL_DATABASE,
-// 		user: process.env.MYSQL_USER,
-// 		password: process.env.MYSQL_PASSWORD,
-// 	},
-// });
-
-// export default async function executeQuery({query, values}) {
-// 	try {
-// 		const results = await db.transaction().query(query, values).commit(); // commit the transaction and send the query
-
-// 		const [innerArray] = results;
-// 		return innerArray;
-// 	} catch (error) {
-// 		console.error("MySQL Error:", error);
-// 		return {error};
-// 	}
-// }
-
-// Get the client
+// This file is used to create a connection to the MySQL database and execute queries.
+// The executeQuery function is used to execute queries and return the results. The connection is created using the mysql2/promise package and the environment variables for the database connection details are read from the .env file.
 import mysql from "mysql2/promise";
 
 // Create the connection to database
@@ -37,7 +14,6 @@ const connection = await mysql.createConnection({
 export default async function executeQuery({query, values}) {
 	try {
 		const [results] = await connection.query(query, values);
-		console.log("results", results); // results contains rows returned by server
 		return results;
 	} catch (error) {
 		console.error("MySQL Error:", error);
