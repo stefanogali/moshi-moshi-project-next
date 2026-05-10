c8227c09cb3be26522e9cb61a74efb9493d03504
Stefano Galiffa <paninos79@yahoo.it>
2026-05-10 15:28:55 +0100

    Upgrade storybook

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@ const path = require("path");
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-	eslint: {
-		ignoreDuringBuilds: true,
-	},
 	sassOptions: {
 		quietDeps: true, // hides deprecation warnings from node_modules
 	},
